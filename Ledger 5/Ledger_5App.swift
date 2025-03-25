e45c274e54fc366999ca51d641bfc9e14dd268e2
Gabe Witcher <gabewitcher@gmail.com>
2025-03-25 15:33:36 -0700

    Initial Commit

---
+++ Ledger 5/Ledger_5App.swift
@@ -0,0 +1,17 @@
+//
+//  Ledger_5App.swift
+//  Ledger 5
+//
+//  Created by Gabe Witcher on 3/25/25.
+//
+
+import SwiftUI
+
+@main
+struct Ledger_5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
